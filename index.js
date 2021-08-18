d347d3501bb164c91ca60a2eda678ad1c1e8b1e3
Matias Vieitez <matiasvieitez18@gmail.com>
2021-08-18 20:27:29 -0300

    mi api

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.json())
+
+app.listen(port, () => {
+
+    console.log('server runing on port :', port)
+
+})
+
+
+
+let Personas = [
+
+    {
+        id: 1,
+        Name: 'Matias',
+        Surname: 'Vieitez',
+        Age: 21,
+    },
+    {
+        id: 2,
+        Name: 'Raul',
+        Surname: 'Vieitez',
+        Age: 53,
+    },
+    {
+        id: 3,
+        Name: 'Valeria',
+        Surname: 'Gianarello',
+        Age: 48,
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>hola mundo</h1>')
+})
+
+app.get('/Api/Personas', (req, res) => {
+
+    res.json(Personas)
+
+})
+
+app.get('/Api/Personas/:id', (req, res) => {
+
+    const id = Number(req.params.id)
+    const persona = Personas.find(persona => persona.id === id)
+    if (persona) {
+        res.json(persona)
+    } else {
+        res.send('<h2>No existe una persona con esa id</h2 > ')
+    }
+
+})
+
+
+app.delete('/Api/Personas/:id', (req, res) => {
+
+    const id = Number(req.params.id)
+    Personas = Personas.filter(persona => persona.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/Api/Personas', (req, res) => {
+
+    const persona = req.body;
+
+    const ids = Personas.map(persona => persona.id)
+    const maxId = Math.max(...ids)
+
+    const nPersona = {
+
+        id: maxId + 1,
+        Name: persona.Name,
+        Surname: persona.Surname,
+        Age: persona.Age
+
+    }
+
+    Personas = [...Personas, nPersona] //destructuring
+
+    res.json(nPersona)
+
+})
