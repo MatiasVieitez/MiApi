d08b961b03f77314b1da832733933dda86a836c3
Matias Vieitez <matiasvieitez18@gmail.com>
2021-08-19 19:36:42 -0300

    olvide el procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const consoleMid = require('./consoleMiddleware')
 const cors = require('cors')
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 app.use(express.json())
 
